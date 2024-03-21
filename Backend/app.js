c8bb1f670c207809d5765412dc9b7b9b8c8515b0
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-03-21 12:00:35 +0530

    Testing adding data to the MongoDB connection

--- Backend/app.js
+++ Backend/app.js
@@ -14,6 +14,36 @@ mongoose.connect("mongodb+srv://udeshidumina:udesh123@cluster0.4jg3pk6.mongodb.n
         console.log("error")
     }
 })
+
+//schema
+
+const sch={
+    firstname:String,
+    lastname:String,
+    email:String,
+    password:String
+}
+const model=mongoose.model("user", sch);
+
+//post
+app.post("/post",async(req,res)=>{
+    console.log("inside post function");
+
+    const data = new model({
+         firstname:req.body.firstname,
+         lastname:req.body.lastname,
+         email:req.body.email,
+         password:req.body.password
+    });
+    console.log(data);
+
+    const val = await data.save();
+    res.json(val);
+
+
+})
+
 app.listen(3001,()=>{
     console.log("on port 3001")
+
 })
