ba1ee05971fd2b4af2edbf32074df7bfbe752c94
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-03-27 14:38:18 +0530

    adding

---
+++ frontend/note_master/src/app/components/Footer.js
@@ -0,0 +1,12 @@
+
+
+const Footer = ()=> {
+    return (
+        <footer>
+            <p>&copy; 2024 Udesh Indumina. All rights reserved.</p>
+        </footer>
+    );
+}
+  
+  export default Footer;
+  
