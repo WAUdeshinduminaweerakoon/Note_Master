124cb8d8b69630010b2f0abd6e75f0b54999b385
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-03-29 08:38:23 +0530

    text overflow-auto

--- frontendNew/note_master_frontend/src/Components/Note.jsx
+++ frontendNew/note_master_frontend/src/Components/Note.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const Note = ({id, text, editHandler, deleteHadler}) => {
+const Note = ({ id, text, editHandler, deleteHadler }) => {
   return (
     <div className='relative items-center justify-center w-full p-3 md:w-1/4'>
-         <div className='w-full h-full p-8 bg-transparent border border-gray-600 rounded-md bg-stone-300'>
-            <div className='w-full h-full p-5 bg-transparent border border-gray-600 rounded-md focus:outline-none bg-slate-50'>   
-                {text}
-            </div>
+      <div className='w-full h-full p-8 bg-transparent border border-gray-600 rounded-md bg-stone-300'>
+        <div className='w-full h-40 p-5 overflow-auto bg-transparent border border-gray-600 rounded-md focus:outline-none bg-slate-50'>
+          {text}
+        </div>
         <div>
-            <button  className='px-2 py-1 text-black bg-teal-300 rounded-md hover:bg-blue-200 focus:outline-none' onClick={() => deleteHadler(id)}>Delete</button>
-            <button  className='px-2 py-1 text-black bg-teal-300 rounded-md hover:bg-blue-200 focus:outline-none' onClick={() => editHandler(id, text)}>Edit</button>
+          <button className='px-2 py-1 text-black bg-teal-300 rounded-md hover:bg-blue-200 focus:outline-none' onClick={() => deleteHadler(id)}>Delete</button>
+          <button className='px-2 py-1 text-black bg-teal-300 rounded-md hover:bg-blue-200 focus:outline-none' onClick={() => editHandler(id, text)}>Edit</button>
         </div>
-        </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Note;
